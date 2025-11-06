f313ef3cd60e331daae20d6953c2f51f00a6e951
Ovidio Juan <149029398+XxTimaeusxX@users.noreply.github.com>
2025-11-06 11:59:08 -0500

    pause menu navigation setup

--- KitsuneCards/Assets/Scripts/Menu/MainMenuController.cs
+++ KitsuneCards/Assets/Scripts/Menu/MainMenuController.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    public GameObject pauseMenu;
+    public GameObject OptionsMenu;
+    public void Start()
+    {
+        pauseMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+    }
     public void LoadRegularmode()
     {
         SceneManager.LoadScene("RegularModeScene");
@@ -25,6 +33,23 @@ public class MainMenuController : MonoBehaviour
     public void LoadMainMenu()
     {
        SceneManager.LoadScene("MainMenu");
+    }
+    /////////////// Pause Menu Functions //////////////
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+        Time.timeScale = 0f; // Pause the game
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1; // Resume the game
+    }
+    public void LoadOptions()
+    {
+        pauseMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
     }
     // Optional: Quit button
     public void QuitGame()
